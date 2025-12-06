f54ee23b6119273e7bd7e0a1a37a719c049e7c1e
Dena <denasatwikaa@gmail.com>
2025-12-06 20:17:28 +0800

    first commit

---
+++ src/types/global.d.ts
@@ -0,0 +1,8 @@
+// src/types/global.d.ts
+
+// Mengatasi error ts(7016) untuk eslint-config-airbnb-base
+declare module 'eslint-config-airbnb-base';
+
+// Tambahkan juga deklarasi untuk paket konfigurasi lainnya (jika Anda menggunakan airbnb-typescript)
+declare module 'eslint-config-airbnb-typescript/base';
+declare module '@eslint/compat'; 
